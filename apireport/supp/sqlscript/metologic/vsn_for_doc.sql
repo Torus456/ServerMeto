cf315c757480e9bd709bdbc80ed9beb6eb949796
Torus456 <odilich@yandex.ru>
2021-12-23 10:06:43 +0500

    Fix for vsn from Anna Lavrova

--- apireport/supp/sqlscript/metologic/vsn_for_doc.sql
+++ apireport/supp/sqlscript/metologic/vsn_for_doc.sql
@@ -201,7 +201,9 @@ select distinct  bsdv.mlt_id,
                        else replace(regexp_replace(to_char(vsn.valnum), '^\.', '0.'), '.', ',') 
                   end
         end value,
-        sp_acceptor.return_values (bsdv.mlt_id, bsdv.clf_id, bsdv.cls_id, bsdv.prj_id, bsdv.dvs_id, bsdv.sgn_id, vsn.vsn_id,1) symsgn,
+        case when bsdv.only_dop = 2 
+             then nvl(sp_acceptor.return_values (bsdv.mlt_id, bsdv.clf_id, bsdv.cls_id, bsdv.prj_id, bsdv.dvs_id, bsdv.sgn_id, vsn.vsn_id,1), sp_acceptor.return_values (bsdv.mlt_id, bsdv.clf_id, bsdv.cls_id, bsdv.prj_id, bsdv.dvs_id, bsdv.sgn_id, vsn.vsn_id,0))
+             else sp_acceptor.return_values (bsdv.mlt_id, bsdv.clf_id, bsdv.cls_id, bsdv.prj_id, bsdv.dvs_id, bsdv.sgn_id, vsn.vsn_id,1) end symsgn,
         case when vsn.vsn_id = 0 then vsn.symsgn
              else case when bsdv.valtype = 0 then vsn.valchar
                        else replace(regexp_replace(to_char(vsn.valnum), '^\.', '0.'), '.', ',') 
