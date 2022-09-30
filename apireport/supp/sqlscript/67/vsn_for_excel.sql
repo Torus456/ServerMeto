1b50cea7f6d1cc85f81982e600caa1e6a094c40b
Torus456 <odilich@yandex.ru>
2022-09-30 11:58:00 +0500

    fix resolve duble with units

--- apireport/supp/sqlscript/67/vsn_for_excel.sql
+++ apireport/supp/sqlscript/67/vsn_for_excel.sql
@@ -247,7 +247,7 @@ unq as
 				 replace_code_name(oclp.mlt_id, oclp.clf_id, oclp.cls_id, zdvs.cfv_id, oclp.fname, 1) as fname,
 				 replace_code_name(oclp.mlt_id, oclp.clf_id, oclp.cls_id, zdvs.cfv_id, oclp.sname, 1) as sname, 
 				 case when oclp.sname||oclp.fname like '%?%' then 'Ненормализовано' else 'Нормализовано' end normal,
-                 case when count( distinct obj.obj_id) over (PARTITION BY oclp.sname) > 1 and oclp.sname||oclp.fname not like '%?%' then 'Дубль' else null end  duble,                 
+                 case when count( distinct obj.obj_id) over (PARTITION BY oclp.sname, nvl(ums494.code, zdvs.ei_norm)) > 1 and oclp.sname||oclp.fname not like '%?%' then 'Дубль' else null end  duble,                 
 				 sdv.code, 
 				 sdv.name, 
 				 e.val 
